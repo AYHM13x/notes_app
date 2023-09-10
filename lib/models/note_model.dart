9df36a7bf8e4b570d7c06668d41d9f07d93c0d5a
AYHM13x <ayhmalakel1995@gmail.com>
2023-09-10 20:58:58 +0300

    add note model adopter by build_runner build

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,15 +1,23 @@
-class NoteModel {
-  final String? title, content, dateTime;
-  final int? Color;
+import 'package:hive_flutter/adapters.dart';
+import 'package:hive/hive.dart';
+
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
+  final String? title;
+  @HiveField(1)
+  final String? content;
+  @HiveField(2)
+  final String? dateTime;
+  @HiveField(3)
+  final int? color;
 
   NoteModel({
     required this.title,
     required this.content,
     required this.dateTime,
-    required this.Color,
+    required this.color,
   });
-
-  // factory NoteModel.fromjson(jsonData) {
-  //   return NoteModel(title: );
-  // }
 }
